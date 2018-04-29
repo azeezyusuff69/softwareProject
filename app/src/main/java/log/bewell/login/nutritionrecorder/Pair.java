bcce492a1badd0116dcf697f7b03f92575ba1763
Azeez Yusuff <ABDULAZEEZ-YUSUFF@HOTMAIL.COM>
2018-04-29 15:59:35 +0100

    New Nutrition sector with graph

---
+++ app/src/main/java/log/bewell/login/nutritionrecorder/Pair.java
@@ -0,0 +1,12 @@
+package log.bewell.login.nutritionrecorder;
+
+import java.util.Date;
+
+public class Pair {
+    Date day;
+    double calories;
+    Pair(Date day, double calories) {
+        this.day = day;
+        this.calories = calories;
+    }
+}
